05186a7e740949bb172851e837006b291534bee0
nischalmudennavar <nischalmudennavar@gmail.com>
2024-05-23 09:20:53 +0530

    added doc

--- Blind75/#121 Best time to buy and sell stock/index.js
+++ Blind75/#121 Best time to buy and sell stock/index.js
@@ -8,13 +8,24 @@ const solution = (nums) => {
     // Iterate through the array, but stop one index before the end
     if (nums[left] < nums[right]) {
       // If the value at the left pointer is less than the value at the right pointer
+      console.log(
+        `${nums[left]} is at ${left} , and ${nums[right]} is at ${right} `,
+      );
       currentMaxProfit = Math.max(currentMaxProfit, nums[right] - nums[left]); // Update the maximum profit if the current profit is greater
-      right++; // Move the right pointer forward
+      console.log(
+        "current max profit after comparison with previous is : ",
+        currentMaxProfit,
+      );
     } else {
+      console.log(
+        "\n left pointer moved to right, and right pointer moved to next since left > right and we dont want that",
+      );
       left = right; // Move the left pointer to the right pointer
-      right++; // Move the right pointer forward
     }
+    right++;
   }
 
   return currentMaxProfit; // Return the maximum profit
 };
+
+console.log("\n \n solution is ", solution([7, 1, 0, 3, 6, 4]));
